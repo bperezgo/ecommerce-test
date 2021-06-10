a2e4f87610eb87b4b6ab3f5e1ca8b65b20b4d555
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-09 22:06:26 -0500

    feat: api implementation in the server side

--- src/server/routes/items.route.ts
+++ src/server/routes/items.route.ts
@@ -5,11 +5,11 @@ import { amountAndDecimals, mostSearchedCategory } from '../../utils/server';
 import { MlSites, MlCategories } from '../../@types';
 
 export const itemsRouter = Router();
+const apiInstance = api({ baseURL: ML_API_URL });
 // ?q=​:query
 itemsRouter.get('/items', async (req, res, next) => {
   try {
     const { query } = req;
-    const apiInstance = api({ baseURL: ML_API_URL });
     const { data } = await apiInstance.get<MlSites.Results>(
       '/sites/MLA/search',
       {
@@ -27,10 +27,7 @@ itemsRouter.get('/items', async (req, res, next) => {
       categories = category.path_from_root.map((element) => element.name);
     }
     res.status(200).json({
-      author: {
-        name: AUTHOR.name,
-        lastname: AUTHOR.lastname,
-      },
+      author: { ...AUTHOR },
       categories,
       items: data.results.map((result: MlSites.Result) => ({
         id: result.id,
@@ -47,5 +44,36 @@ itemsRouter.get('/items', async (req, res, next) => {
     });
   } catch (err) {
     console.log('Error: ', err);
+  }
+});
+
+itemsRouter.get('/items/:itemId', async (req, res, next) => {
+  try {
+    const { itemId } = req.params;
+    const { data: item } = await apiInstance.get<MlSites.Result>(
+      `/items/${itemId}`
+    );
+    const { data: description } = await apiInstance.get<{ plain_text: string }>(
+      `/items/${itemId}/description`
+    );
+    res.status(200).json({
+      author: { ...AUTHOR },
+      item: {
+        id: itemId,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount: amountAndDecimals(`${item.price}`)[0],
+          decimals: amountAndDecimals(`${item.price}`)[1],
+        },
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping,
+        sold_quantity: item.sold_quantity,
+        description: description.plain_text,
+      },
+    });
+  } catch (err) {
+    console.log(err);
   }
 });
