46a68c93aa73468232dd2140b38e3f7ce844dd0b
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-14 12:47:29 -0500

    feat: intersection observer in individual product

--- src/components/Products/product.tsx
+++ src/components/Products/product.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { ProductsResponse } from '../../@types';
 import { formatPrice } from '../../utils/front';
+import { useViewIntersection } from '../../hooks';
 
 export const Product = ({
   id,
@@ -14,24 +15,52 @@ export const Product = ({
   const handleClick = (id: string) => {
     history.push(`/items/${id}`);
   };
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== undefined
+        ? window.IntersectionObserver
+        : require('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0];
+        if (isIntersecting) {
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      if (ref.current) {
+        observer.observe(ref.current);
+      }
+    });
+  }, [ref]);
   return (
-    <div className="Product__container" onClick={() => handleClick(id)}>
-      <div className="Product__img_block">
-        <img className="Globals__container_img" src={picture} alt={id} />
-      </div>
-      <div className="Product__description">
-        <div className="Product__price">
-          <p>{formatPrice(price.amount, price.currency)}</p>
-          {free_shipping && (
-            <img src={'assets/ic_shipping.png'} alt="shipping" />
-          )}
-        </div>
-        <p className="Product__title">{title}</p>
-        <p className="Product__title">Completo único</p>
-      </div>
-      <span className="Product__head_note">
-        <p>Alguna parte</p>
-      </span>
+    <div
+      ref={ref}
+      className="Product__container"
+      onClick={() => handleClick(id)}
+    >
+      {show && (
+        <>
+          <div className="Product__img_block">
+            <img className="Globals__container_img" src={picture} alt={id} />
+          </div>
+          <div className="Product__description">
+            <div className="Product__price">
+              <p>{formatPrice(price.amount, price.currency)}</p>
+              {free_shipping && (
+                <img src={'assets/ic_shipping.png'} alt="shipping" />
+              )}
+            </div>
+            <p className="Product__title">{title}</p>
+            <p className="Product__title">Completo único</p>
+          </div>
+          <span className="Product__head_note">
+            <p>Alguna parte</p>
+          </span>
+        </>
+      )}
     </div>
   );
 };
