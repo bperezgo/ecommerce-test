5e7717f1070dbb73a1ef9ca4a69d7f00b9a7526a
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-15 01:28:30 -0500

    feat: Products in another folder

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Product } from './Product';
+import { Product } from '../Product';
 import { ProductsResponse } from '../../@types';
 
 type ProductListProps = {
@@ -9,8 +9,7 @@ type ProductListProps = {
 export const ProductList = ({ products }: ProductListProps) => (
   <>
     {products.map((product) => (
-      <div key={product.id}></div>
-      // <Product key={product.id} {...product} />
+      <Product key={product.id} {...product} />
     ))}
   </>
 );
