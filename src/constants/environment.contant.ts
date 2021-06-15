6f5dda5b21da628545ec4a3564399c0548cfd454
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-15 01:34:40 -0500

    fix: port environment was changed in configuration

--- src/constants/environment.contant.ts
+++ src/constants/environment.contant.ts
@@ -5,7 +5,7 @@ type Server = {
 };
 
 export const SERVER: Server = {
-  PORT: process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3000,
+  PORT: process.env.PORT ? parseInt(process.env.PORT) : 3000,
   HOST: process.env.SERVER_HOST || '',
   NODE_ENV:
     process.env.NODE_ENV === 'production' ? 'production' : 'development',
