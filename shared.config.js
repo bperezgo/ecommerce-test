26c3293af2af1c20ab67949c7f0c0a49c3416e7f
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-14 23:36:28 -0500

    refactor: deleted aliases configuration

--- shared.config.js
+++ shared.config.js
@@ -3,11 +3,11 @@ const path = require('path');
 const moduleResolve = {
   extensions: ['.ts', '.tsx', '.js'],
   alias: {
-    '@components': path.resolve(__dirname, './src/components/'),
-    '@containers': path.resolve(__dirname, './src/containers/'),
-    '@styles': path.resolve(__dirname, './src/styles/'),
-    '@assets': path.resolve(__dirname, './src/assets/'),
-    '@utils': path.resolve(__dirname, './src/utils/'),
+    // '@components': path.resolve(__dirname, './src/components/'),
+    // '@containers': path.resolve(__dirname, './src/containers/'),
+    // '@styles': path.resolve(__dirname, './src/styles/'),
+    // '@assets': path.resolve(__dirname, './src/assets/'),
+    // '@utils': path.resolve(__dirname, './src/utils/'),
     'react-dom': '@hot-loader/react-dom',
   },
 };
